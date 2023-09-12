2ad454b721e8099337520ba03574c508dfa6abde
mwssy <mokmail070@gmail.com>
2023-09-12 14:53:24 +0900

    class 구현까지

---
+++ README.md
@@ -0,0 +1,13 @@
+## 계산기 만들기 
+
+### 1주차
+- [x] 기본 UI 완성
+- [x] data 속성 추가
+- [x] calculator class 만들기
+- [ ] 계산기 기능 관련 DOM 세팅
+- [ ] 숫자 버튼 기능(onPressNumber) 구현
+
+### 2주차
+- [ ] 사칙연산 버튼 기능(onPressOperation) 구현
+- [ ] onEqual, onReset, onDelete 기능 구현
+- [ ] 데이터 벨리데이션 추가
---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+function Calculator(){
+
+}
+
+const cal = new Calculator()
+*/
+
+class Calculator{
+
+}
+
+const cal = new Calculator()
+
+// 값 표시
+const $previousPreview = document.querySelector('[data-previous-preview]')
+const $currentPreview = document.querySelector(['[data-current-preview]'])
+console.log($previousPreview)
+console.log($currentPreview)
+
+// 숫자, 연산
