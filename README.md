0ba8de915ad84a1bb2c51e9a44edb88f1a360456
mwssy <mokmail070@gmail.com>
2023-09-12 15:07:15 +0900

    계산기 관련 DOM 세팅까지

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [x] 기본 UI 완성
 - [x] data 속성 추가
 - [x] calculator class 만들기
-- [ ] 계산기 기능 관련 DOM 세팅
+- [x] 계산기 기능 관련 DOM 세팅
 - [ ] 숫자 버튼 기능(onPressNumber) 구현
 
 ### 2주차
--- script.js
+++ script.js
@@ -15,7 +15,25 @@ const cal = new Calculator()
 // 값 표시
 const $previousPreview = document.querySelector('[data-previous-preview]')
 const $currentPreview = document.querySelector(['[data-current-preview]'])
-console.log($previousPreview)
-console.log($currentPreview)
+
+// 사칙연산
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiply = document.querySelector('[data-btn-multiply]')
+const $divide = document.querySelector('[data-btn-divide]')
 
 // 숫자, 연산
+const $numbers = document.querySelectorAll('[data-btn-number]')
+const $operations = document.querySelectorAll('[data-btn-operation]')
+
+$numbers.forEach(($numbers) => {
+    $numbers.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+})
+
+$operations.forEach(($operations) => {
+    $operations.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+    })
+})
